03cb8494aac57f0f5d31d940618bf8319bcdc96c
Daniel Ahn <et3rnal.d@gmail.com>
2021-10-12 17:08:21 -0400

    add Bloglist component

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,18 @@
-const Home = () => {
-  const handleClick = () => {
-    alert('AHHHH u clicked me sonnyboy');
-  }
+import { useState } from 'react';
+import Bloglist from './Bloglist';
 
-  return (  
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
+
+  return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
+      <Bloglist blogs={blogs} title="All Blogs"/>
     </div>
   );
-}
- 
+};
+
 export default Home;
